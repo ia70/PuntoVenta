7fcdb1eb2304ba2f519e40a94c2090f8b4f2be82
Daniel Pérez C. <pcd_mac-000100111@hotmail.com>
2014-07-22 11:51:42 -0500

    DepartamentoEditar <- Semiterminado

--- Abarrotes/Departamento/DepartamentoEditar.Designer.vb
+++ Abarrotes/Departamento/DepartamentoEditar.Designer.vb
@@ -22,42 +22,39 @@ Partial Class DepartamentoEditar
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Label2 = New System.Windows.Forms.Label()
+        Me.BCancelar = New System.Windows.Forms.Button()
+        Me.BGuardar = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TDescripcion = New System.Windows.Forms.TextBox()
         Me.Label3 = New System.Windows.Forms.Label()
+        Me.Pic1 = New System.Windows.Forms.PictureBox()
+        Me.TDepartamento = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.CDepartamento = New System.Windows.Forms.ComboBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        CType(Me.Pic1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'Button2
+        'BCancelar
         '
-        Me.Button2.Location = New System.Drawing.Point(199, 150)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(90, 27)
-        Me.Button2.TabIndex = 9
-        Me.Button2.Text = "Cancelar"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.BCancelar.Location = New System.Drawing.Point(251, 212)
+        Me.BCancelar.Name = "BCancelar"
+        Me.BCancelar.Size = New System.Drawing.Size(90, 27)
+        Me.BCancelar.TabIndex = 9
+        Me.BCancelar.Text = "Cancelar"
+        Me.BCancelar.UseVisualStyleBackColor = True
         '
-        'Button1
+        'BGuardar
         '
-        Me.Button1.Location = New System.Drawing.Point(307, 150)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(90, 27)
-        Me.Button1.TabIndex = 8
-        Me.Button1.Text = "Guardar"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(30, 67)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(127, 19)
-        Me.Label2.TabIndex = 6
-        Me.Label2.Text = "Departamento :"
+        Me.BGuardar.Enabled = False
+        Me.BGuardar.Location = New System.Drawing.Point(356, 212)
+        Me.BGuardar.Name = "BGuardar"
+        Me.BGuardar.Size = New System.Drawing.Size(90, 27)
+        Me.BGuardar.TabIndex = 8
+        Me.BGuardar.Text = "Guardar"
+        Me.BGuardar.UseVisualStyleBackColor = True
         '
         'Label1
         '
@@ -69,59 +66,115 @@ Partial Class DepartamentoEditar
         Me.Label1.TabIndex = 5
         Me.Label1.Text = "Editar departamento"
         '
-        'ComboBox1
+        'TDescripcion
         '
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(155, 64)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(245, 26)
-        Me.ComboBox1.TabIndex = 10
-        '
-        'TextBox1
-        '
-        Me.TextBox1.Location = New System.Drawing.Point(226, 102)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(174, 26)
-        Me.TextBox1.TabIndex = 12
+        Me.TDescripcion.Location = New System.Drawing.Point(103, 37)
+        Me.TDescripcion.MaxLength = 250
+        Me.TDescripcion.Multiline = True
+        Me.TDescripcion.Name = "TDescripcion"
+        Me.TDescripcion.Size = New System.Drawing.Size(308, 59)
+        Me.TDescripcion.TabIndex = 14
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(30, 105)
+        Me.Label3.Location = New System.Drawing.Point(-3, 37)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(198, 18)
-        Me.Label3.TabIndex = 11
-        Me.Label3.Text = "Nombre del departamento :"
+        Me.Label3.Size = New System.Drawing.Size(100, 18)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "Descripción :"
+        '
+        'Pic1
+        '
+        Me.Pic1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Pic1.Location = New System.Drawing.Point(386, 2)
+        Me.Pic1.Name = "Pic1"
+        Me.Pic1.Size = New System.Drawing.Size(25, 25)
+        Me.Pic1.TabIndex = 12
+        Me.Pic1.TabStop = False
+        '
+        'TDepartamento
+        '
+        Me.TDepartamento.Location = New System.Drawing.Point(201, 2)
+        Me.TDepartamento.Name = "TDepartamento"
+        Me.TDepartamento.Size = New System.Drawing.Size(179, 26)
+        Me.TDepartamento.TabIndex = 11
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(-3, 5)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(198, 18)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Nombre del departamento :"
+        '
+        'CDepartamento
+        '
+        Me.CDepartamento.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.CDepartamento.FormattingEnabled = True
+        Me.CDepartamento.Location = New System.Drawing.Point(154, 62)
+        Me.CDepartamento.Name = "CDepartamento"
+        Me.CDepartamento.Size = New System.Drawing.Size(201, 26)
+        Me.CDepartamento.TabIndex = 15
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(32, 65)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(116, 18)
+        Me.Label4.TabIndex = 16
+        Me.Label4.Text = "Departamento :"
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.TDepartamento)
+        Me.Panel1.Controls.Add(Me.Pic1)
+        Me.Panel1.Controls.Add(Me.TDescripcion)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Enabled = False
+        Me.Panel1.Location = New System.Drawing.Point(36, 101)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(415, 105)
+        Me.Panel1.TabIndex = 17
         '
         'DepartamentoEditar
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 18.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(419, 189)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.ComboBox1)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.Label2)
+        Me.ClientSize = New System.Drawing.Size(456, 248)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.CDepartamento)
+        Me.Controls.Add(Me.BCancelar)
+        Me.Controls.Add(Me.BGuardar)
         Me.Controls.Add(Me.Label1)
         Me.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
-        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "DepartamentoEditar"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Editar departamento"
+        CType(Me.Pic1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents Button2 As System.Windows.Forms.Button
-    Friend WithEvents Button1 As System.Windows.Forms.Button
-    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents BCancelar As System.Windows.Forms.Button
+    Friend WithEvents BGuardar As System.Windows.Forms.Button
     Friend WithEvents Label1 As System.Windows.Forms.Label
-    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TDescripcion As System.Windows.Forms.TextBox
     Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Pic1 As System.Windows.Forms.PictureBox
+    Friend WithEvents TDepartamento As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents CDepartamento As System.Windows.Forms.ComboBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
 End Class
--- Abarrotes/Departamento/DepartamentoEditar.vb
+++ Abarrotes/Departamento/DepartamentoEditar.vb
@@ -1,3 +1,73 @@
 ﻿Public Class DepartamentoEditar
+    Private tb As New DataTable
 
+    Private Sub TDepartamento_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TDepartamento.KeyPress
+        If e.KeyChar = Chr(13) And TDepartamento.TextLength > 3 Then
+            TDescripcion.Focus()
+        Else
+            e = Validar_NombreNumero(e, TDepartamento.Text)
+        End If
+    End Sub
+
+    Private Sub TDepartamento_TextChanged(sender As Object, e As EventArgs) Handles TDepartamento.TextChanged
+        If TDepartamento.TextLength > 3 Then
+            Dim db As New DB
+            Dim Tabla As New DataTable
+
+            Tabla = db.Consultar("select * from departamento where Nombre='" & TDepartamento.Text & "'")
+            If Tabla.Rows.Count > 0 And Not TDepartamento.Text = CDepartamento.Text Then
+                Pic1.BackgroundImage = My.Resources.Error_4
+                BGuardar.Enabled = False
+            Else
+                Pic1.BackgroundImage = My.Resources.Select_2
+                BGuardar.Enabled = True
+            End If
+        Else
+            Pic1.BackgroundImage = Nothing
+            BGuardar.Enabled = False
+        End If
+    End Sub
+
+    Private Sub BGuardar_Click(sender As Object, e As EventArgs) Handles BGuardar.Click
+        Dim db As New DB
+
+        If db.Ejecutar("update departamento set Nombre='" & TDepartamento.Text & "',Descripcion='" & TDescripcion.Text & "' where Nombre='" & CDepartamento.Text & "'") Then
+            MsgBox("Registro actualizado correctamente!", vbInformation, "Información")
+            TDepartamento.Text = ""
+            TDescripcion.Text = ""
+            DepartamentoEditar_Load(sender, e)
+            Panel1.Enabled = False
+            TDepartamento.Focus()
+        Else
+            MsgBox("Error al intentar guardar registro!", vbCritical, "Información")
+        End If
+    End Sub
+
+    Private Sub DepartamentoEditar_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim db As New DB
+
+        tb = db.Consultar("select * from departamento")
+        CDepartamento.Items.Clear()
+        If tb.Rows.Count > 0 Then
+            For Each Fila As DataRow In tb.Rows
+                CDepartamento.Items.Add(Fila.Item(1).ToString)
+            Next
+        Else
+            MsgBox("Aun no hay ningun departamento registrado!", vbExclamation, "Información")
+            CDepartamento.Enabled = False
+        End If
+    End Sub
+
+    Private Sub CDepartamento_SelectedIndexChanged(sender As Object, e As EventArgs) Handles CDepartamento.SelectedIndexChanged
+
+        For Each Fila As DataRow In tb.Rows
+            If Fila.Item(1).ToString = CDepartamento.Text Then
+                TDepartamento.Text = Fila.Item(1).ToString
+                TDescripcion.Text = Fila.Item(2).ToString
+                Exit For
+            End If
+        Next
+        Panel1.Enabled = True
+        BGuardar.Enabled = True
+    End Sub
 End Class
